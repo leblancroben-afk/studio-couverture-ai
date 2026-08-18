1f8fc98a9a71af183ff71d5993ebed522c3f541f
leblancroben-afk <leblancroben@gmail.com>
2026-08-18 07:45:39 -0400

    Set live Cloudflare Worker URL

--- assets/js/worker-config.js
+++ assets/js/worker-config.js
@@ -3,5 +3,5 @@
  * Voir cf-worker/worker.js + DOCUMENTATION.html pour le déploiement.
  * Exemple : "https://studio-couverture-worker.VOTRE-SOUS-DOMAINE.workers.dev"
  */
-const WORKER_URL = "";
+const WORKER_URL = "https://dry-hall-3c93.leblancroben.workers.dev";
 const WORKER_IS_CONFIGURED = WORKER_URL.length > 0;
